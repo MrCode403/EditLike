88fab32c7b6544990f6e8f69d5e8d4626c729f82
datechies <maildatechies@gmail.com>
2022-08-21 05:25:29 +0530

    implemented view binding in TemplateActivity.java

--- app/src/main/java/com/editlike/app/TemplateActivity.java
+++ app/src/main/java/com/editlike/app/TemplateActivity.java
@@ -5,36 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.widget.*;
 import android.widget.LinearLayout;
 import android.os.*;
-import android.app.*;
-import android.view.Window;
-import android.view.WindowManager;
 import android.graphics.Color;
 import android.view.*;
 import android.view.View.*;
 import android.content.Intent;
+import com.editlike.app.TemplateActivity;
 import io.michaelrocks.paranoid.Obfuscate;
+import com.editlike.app.databinding.TemplateBinding;
 
 @Obfuscate
 public class TemplateActivity extends AppCompatActivity {
 
-  private LinearLayout base;
-  private LinearLayout template1;
-  private LinearLayout template2;
+  private TemplateBinding binding;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     
-    // Set Layout template.xml from (res/layout/template.xml)
-    setContentView(R.layout.template);
-    
-    // Find Views by Id
-    base = findViewById(R.id.base);
-    template1 = findViewById(R.id.template1);
-    template2 = findViewById(R.id.template2);
-    
+    binding = TemplateBinding.inflate(getLayoutInflater());
+    setContentView(binding.getRoot());
+
     // template1 button on clicked
-    template1.setOnClickListener(
+    binding.template1.setOnClickListener(
         new View.OnClickListener() {
           @Override
           public void onClick(View view) {
@@ -45,9 +37,9 @@ public class TemplateActivity extends AppCompatActivity {
             startActivity(MainPage);
           }
         });
-        
-    // template2 buuton on clicked    
-    template2.setOnClickListener(
+
+    // template2 buuton on clicked
+    binding.template2.setOnClickListener(
         new View.OnClickListener() {
           @Override
           public void onClick(View view) {
@@ -63,20 +55,18 @@ public class TemplateActivity extends AppCompatActivity {
   @Override
   public void onStart() {
     super.onStart();
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-      Window window = this.getWindow();
-      window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-      window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-      window.setStatusBarColor(Color.parseColor("#7289DA"));
-      window.setNavigationBarColor(Color.parseColor("#353c57"));
-    }
+    Window window = this.getWindow();
+    window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+    window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+    window.setStatusBarColor(Color.parseColor("#7289DA"));
+    window.setNavigationBarColor(Color.parseColor("#353c57"));
 
     android.graphics.drawable.GradientDrawable gd001 =
         new android.graphics.drawable.GradientDrawable(
             android.graphics.drawable.GradientDrawable.Orientation.TOP_BOTTOM,
             new int[] {0xFF7289DA, 0xFF353c57});
     gd001.setCornerRadius(0f);
-    base.setBackgroundDrawable(gd001);
+    binding.base.setBackgroundDrawable(gd001);
   }
 
   @Override
