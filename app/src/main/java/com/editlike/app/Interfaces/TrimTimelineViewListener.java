717f91772d55898db949635f1a1a085014cefd9c
datechies <maildatechies@gmail.com>
2022-08-23 23:14:08 +0530

    Seperated Video Trimer from MainActivity

---
+++ app/src/main/java/com/editlike/app/Interfaces/TrimTimelineViewListener.java
@@ -0,0 +1,7 @@
+package com.editlike.app.Interfaces;
+
+public interface TrimTimelineViewListener {
+  void onLeftProgressChanged(float progress);
+
+  void onRightProgressChanged(float progress);
+}
